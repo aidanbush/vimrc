1eeeddd5f7e93903dd58b627e1ed7cf654dc4d70
aidan <aidandbush@gmail.com>
2017-07-15 22:05:16 -0600

    add basic install script

---
+++ install-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# copy vimrc file over
+cp .vimrc ~/
+
+# get colour file
+git clone https://github.com/aidanbush/vim-colors-tayra.git
+
+if [ ! -d ~/.vim/ ]; then
+    mkdir ~/.vim
+fi
+
+if [ ! -d ~/.vim/colors/ ]; then
+    mkdir ~/.vim/colors
+fi
+
+echo "copying over tayra.vim colors file"
+cp vim-colors-tayra/colors/tayra.vim
+
+# copy gitconfig over
+cp .gitconfig ~/
